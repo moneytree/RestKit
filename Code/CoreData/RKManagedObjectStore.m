97ed12090378b365c024cb29d8bc43f4f97dcb34
Rendy Pranata <rpranata@moneytree.jp>
2014-08-27 16:05:38 +0900

    Hopefully the last one.

--- Code/CoreData/RKManagedObjectStore.m
+++ Code/CoreData/RKManagedObjectStore.m
@@ -336,7 +336,10 @@ static char RKManagedObjectContextChangeMergingObserverAssociationKey;
 - (BOOL)resetPersistentStores:(NSError **)error
 {
     [self.mainQueueManagedObjectContext reset];
-    [self.persistentStoreManagedObjectContext reset];
+    [self.persistentStoreManagedObjectContext performBlockAndWait:^{
+        [self.persistentStoreManagedObjectContext reset];
+    }];
+    
     
     NSError *localError;
     for (NSPersistentStore *persistentStore in self.persistentStoreCoordinator.persistentStores) {
